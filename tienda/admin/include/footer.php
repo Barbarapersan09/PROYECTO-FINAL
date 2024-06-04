f9ba7bdb64fc18c222fb29c25512891f9bd7bcff
Barbarapersan09 <barbarapersan09@gmail.com>
2024-06-04 18:38:30 +0200

    completo

---
+++ tienda/admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2024 El Rincón de Bárbara.</a> </b> Todos los derechos reservados.
+		</div>
+	</div>
---
+++ tienda/includes/brands-slider.php
@@ -0,0 +1,55 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Marcas Reconocidas</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="https://www.planetadelibros.com/editorial/editorial-planeta/8" class="image">
+						<img data-echo="brandsimage/Planeta.ico" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="https://www.planetadelibros.com/editorial/espasa/5" class="image">
+						<img data-echo="brandsimage/espasa.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="https://www.planetadelibros.com/editorial/booket/22" class="image">
+						<img data-echo="brandsimage/booket.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="https://www.planetadelibros.com/editorial/ediciones-destino/7" class="image">
+						<img data-echo="brandsimage/destino.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="https://www.planetadelibros.com/editorial/esencia/24" class="image">
+						<img data-echo="brandsimage/esencia.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="https://www.planetadelibros.com/editorial/planeta-comic/54" class="image">
+						<img data-echo="brandsimage/planetaComic.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/dell.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+	</div><!-- /.logo-slider-inner -->
+	
+</div>
+<!-- /.logo-slider -->
